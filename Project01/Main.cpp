bf828688e62162c44437fb468bb10b509d29550c
Евгений Шкумат <kotbmasle@gmail.com>
2024-03-25 12:47:24 +0300

    add class SteamUser

--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
 
+class SteamUser {
+public:
+	string name;
+	double balance;
+	bool is_banned;
+	int level;
+	int hour;
+};
+
 int main() {
-	
+	SteamUser user1;
+
+	user1.name = "MrFly";
+	user1.balance = 17.23;
+	user1.is_banned = false;
+	user1.level = 10;
+	user1.hour = 561;
+
 	return 0;
 }
