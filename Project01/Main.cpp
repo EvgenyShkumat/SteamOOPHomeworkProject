f6d8ea086434ae61b8318608d41e1673209ed3cd
Евгений Шкумат <kotbmasle@gmail.com>
2024-03-25 12:56:46 +0300

    add replenish and getInfo methods

--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class SteamUser {
@@ -8,6 +9,16 @@ public:
 	bool is_banned;
 	int level;
 	int hour;
+
+	string getInfo() {
+		return "Name: " + name + "\nBalance: " + to_string(balance) +
+			"\nSteam account level: " + to_string(level) + "\nTotal hours spent: " +
+			to_string(hour) + " hours\nIs banned: " + (is_banned ? "Yes" : "No");
+	}
+
+	void replenish(double amount) {
+		balance += amount;
+	}
 };
 
 int main() {
@@ -18,6 +29,10 @@ int main() {
 	user1.is_banned = false;
 	user1.level = 10;
 	user1.hour = 561;
+
+	cout << user1.getInfo() << endl;
+	user1.replenish(5.71);
+	cout << user1.getInfo();
 
 	return 0;
 }
